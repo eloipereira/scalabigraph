7e7eb4edc194607395a93a201799e499475231a2
Eloi Pereira <eloipereira@gmail.com>
2016-04-10 18:36:02 +0100

    introduce place graph

--- src/main/scala/bigraph/Bigraph.scala
+++ src/main/scala/bigraph/Bigraph.scala
@@ -23,29 +23,43 @@ sealed trait Site{
 object Site{
   def apply(i: => Int) = new Site{
     lazy val id = i
+    override def toString = "$" + i
   }
   def unapply(s: Site): Option[Int] = Some(s.id)
 }
 
-sealed trait PlaceGraph[A]{
+sealed trait Place[A]{
   def region: Region
-  def graph: Stream[Tree[A \/ Site]]
+  def graph: Stream[Tree[Site \/ A]]
+}
+
+object Place{
+  def apply[A](r: Region, forest: => Stream[Tree[Site \/ A]]): Place[A] = new Place[A]{
+    lazy val region = r
+    lazy val graph = forest
+    override def toString = "<place>"
+  }
+  def unapply[A](p: Place[A]): Option[(Region,Stream[Tree[Site \/ A]])] = Some((p.region,p.graph))
+}
+
+sealed trait PlaceGraph[A]{
+  def places: Stream[Place[A]]
 }
 
 object PlaceGraph extends PlaceGraphFunctions{
-  def apply[A](r: Region, forest: => Stream[Tree[A \/ Site]]): PlaceGraph[A] = new PlaceGraph[A]{
-    lazy val region = r
-    lazy val graph = forest
+  def apply[A](ps: Stream[Place[A]]): PlaceGraph[A] = new PlaceGraph[A]{
+    lazy val places = ps
     override def toString = "<placeGraph>"
   }
-  def unapply[A](p: PlaceGraph[A]): Option[(Region,Stream[Tree[A \/ Site]])] = Some((p.region,p.graph))
+  def unapply[A](pg: PlaceGraph[A]): Option[Stream[Place[A]]] = Some(pg.places)
 }
 
 trait PlaceGraphFunctions {
   implicit val r: Region = Region(0)
   implicit val s: Site = Site(0)
-  def empty[A](implicit r: Region): PlaceGraph[A] = PlaceGraph(r,Stream.empty)
-  def atom[A](n: => A)(implicit r: Region): PlaceGraph[A] = PlaceGraph(r,Stream(Leaf(-\/(n))))
-  def ion[A](n: => A)(implicit r: Region, s: Site): PlaceGraph[A] = PlaceGraph(r,Stream(Node(-\/(n), Stream(Leaf(\/-(s))))))
+  def empty[A](implicit r: Region): PlaceGraph[A] = PlaceGraph(Stream(Place(r,Stream.empty)))
+  def atom[A](n: => A)(implicit r: Region): PlaceGraph[A] = PlaceGraph(Stream(Place(r,Stream(Leaf(\/-(n))))))
+  def ion[A](n: => A)(implicit r: Region, s: Site): PlaceGraph[A] = PlaceGraph(Stream(Place(r,Stream(Node(\/-(n), Stream(Leaf(-\/(s))))))))
   def compose[A](p0: PlaceGraph[A], p1: PlaceGraph[A]): PlaceGraph[A] = ???
+  def product[A](p0: PlaceGraph[A], p1: PlaceGraph[A]): PlaceGraph[A] = ???
 }
