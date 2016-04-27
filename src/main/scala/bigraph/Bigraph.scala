0bcdb4dee2a6b04f5fb4dbac98af1282bce6fdd5
Eloi Pereira <eloipereira@gmail.com>
2016-04-28 00:34:02 +0100

    composition operator

--- src/main/scala/bigraph/Bigraph.scala
+++ src/main/scala/bigraph/Bigraph.scala
@@ -4,17 +4,13 @@ import scalaz._
 import scalaz.Tree._
 import scalaz.std.stream.{streamInstance, streamMonoid}
 
-sealed trait Site
 
-case class Open(id: Int) extends Site {
+case class Site(id: Int) {
   override def toString = "$" + id
-}
-case object Closed extends Site{
-  override def toString = "(-)"
 }
 
 sealed trait PlaceGraph[A]{
-  def forest: Stream[Tree[Site \/ A]]
+  def forest: Stream[Stream[Tree[Site \/ A]]]
   implicit def sh: Show[Site \/ A] = new Show[Site \/ A] {
     override def shows(n: Site \/ A): String = {
       n match {
@@ -23,30 +19,86 @@ sealed trait PlaceGraph[A]{
       }
     }
   }
-  def drawPlaceGraph: String = forest.map(_.drawTree).foldLeft("")(_+_)
+
+  def map[B](f: A => B):PlaceGraph[B] = {
+    val fs = forest.map ((region: Stream[Tree[Site \/ A]]) =>
+    region.map((tree: Tree[Site \/ A]) =>
+    tree.map((n: Site \/ A) =>
+    n.map(f)
+  )
+)
+)
+PlaceGraph(fs)
+}
+
+def drawPlaceGraph: String = drawRegions(forest)
+private def drawRegions(rs: Stream[Stream[Tree[Site\/A]]]): String = rs.length match{
+  case 0 => ""
+  case n => drawRegions(rs.init) + drawTrees(n-1)(rs.last)
+}
+private def drawTrees(region: Int)(ts: Stream[Tree[Site\/A]]): String = "\n#" + region + "[\n" + ts.map(_.drawTree).foldLeft("")(_+_) + "]\n"
 }
 
 object PlaceGraph extends PlaceGraphFunctions{
-  def apply[A](s: Stream[Tree[Site \/ A]]): PlaceGraph[A] = new PlaceGraph[A]{
+  def apply[A](s: Stream[Stream[Tree[Site \/ A]]]): PlaceGraph[A] = new PlaceGraph[A]{
     lazy val forest = s
     override def toString = "<placeGraph>"
   }
-  def unapply[A](pg: PlaceGraph[A]): Option[Stream[Tree[Site \/ A]]] = Some(pg.forest)
+  def unapply[A](pg: PlaceGraph[A]): Option[Stream[Stream[Tree[Site \/ A]]]] = Some(pg.forest)
 }
 
 trait PlaceGraphFunctions {
-  implicit val s: Site = Open(0)
-  def empty[A]: PlaceGraph[A] = PlaceGraph(Stream(Leaf(-\/(Closed))))
-  def permutation[A]: PlaceGraph[A] = PlaceGraph(Stream(Leaf(-\/(Open(1))),Leaf(-\/(Open(0)))))
-  //def join[A]: PlaceGraph[A] = ??? TODO - join doesnt work because a region cannot take two trees. must change the whole definition.
-  def atom[A](n: => A): PlaceGraph[A] = PlaceGraph(Stream(Node(\/-(n), Stream(Leaf(-\/(Closed))))))
-  def ion[A](n: => A)(implicit s: Site): PlaceGraph[A] = PlaceGraph(Stream(Node(\/-(n),Stream(Leaf(-\/(s))))))
+  implicit val s: Site = Site(0)
+  def empty[A]: PlaceGraph[A] = PlaceGraph(Stream(Stream.empty))
+  def permutation[A]: PlaceGraph[A] = PlaceGraph(Stream(Stream(Leaf(-\/(Site(1)))),Stream(Leaf(-\/(Site(0))))))
+  def join[A]: PlaceGraph[A] = PlaceGraph(Stream(Stream(Leaf(-\/(Site(0))),Leaf(-\/(Site(1))))))
+  def atom[A](n: => A): PlaceGraph[A] = PlaceGraph(Stream(Stream(Node(\/-(n), Stream.empty))))
+  def ion[A](n: => A)(implicit s: Site): PlaceGraph[A] = PlaceGraph(Stream(Stream(Node(\/-(n),Stream(Leaf(-\/(s)))))))
   def juxtaposition[A](p0: PlaceGraph[A], p1: PlaceGraph[A]): PlaceGraph[A] = PlaceGraph(p0.forest #::: p1.forest)
-  def composition[A](p0: PlaceGraph[A], p1: PlaceGraph[A]): PlaceGraph[A] = {
-    PlaceGraph(
-      p1.forest map ((t: Tree[Site \/ A]) => t flatMap ((n: Site \/ A) => n match {
-        case -\/(Open(i)) => p0.forest(i)
-        case _ => Leaf(n)
-      })))
+
+  private def findASite[A](loc: TreeLoc[Site \/ A]):Option[TreeLoc[Site \/ A]] = {
+    loc.find { (_:TreeLoc[Site \/ A]).getLabel match {
+      case -\/(Site(i)) => true
+      case _ => false
+    }
   }
+}
+
+private def insertRegionInLocation[A](loc: TreeLoc[Site \/ A],rg0: Stream[Tree[Site \/ A]]):TreeLoc[Site \/ A] = {
+  rg0.length match {
+    case 0 => loc
+    case n => {
+      insertRegionInLocation(loc.insertLeft(rg0.head).right.get,rg0.tail)
+    }
+  }
+}
+
+private def composeRegionsInTree[A](tree: Tree[Site \/ A],rs: Stream[Stream[Tree[Site \/ A]]]): Tree[Site \/ A] = {
+  findASite(tree.loc) match {
+    case None => tree
+    case Some(p) => {
+      p.getLabel match {
+        case -\/(Site(i)) => {
+          if (rs.length > i){
+            val newTree = insertRegionInLocation(p, rs(i)).delete.get.toTree
+            composeRegionsInTree(newTree,rs)
+          } else {
+            tree
+          }
+        }
+        case _ => tree
+      }
+    }
+  }
+}
+
+def composition[A](p0: PlaceGraph[A], p1: PlaceGraph[A]): PlaceGraph[A] = {
+      val fs = p0.forest.map(region =>
+        region.map(t =>
+          composeRegionsInTree(t, p1.forest)
+        )
+      )
+      PlaceGraph(fs)
+}
+
 }
