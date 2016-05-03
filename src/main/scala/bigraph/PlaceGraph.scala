256a9c33a6313fafa00c8163c199c20cba29798b
Eloi Pereira <eloipereira@gmail.com>
2016-05-03 20:49:46 +0100

    fix compose

--- src/main/scala/bigraph/PlaceGraph.scala
+++ src/main/scala/bigraph/PlaceGraph.scala
@@ -12,7 +12,7 @@ import scalaz.std.stream.{streamInstance, streamMonoid}
 import scalaz.Order._
 import scalaz.Equal._
 
-case class Site(id: Int)(implicit val open: Boolean = true) {
+case class Site(id: Int) {
   override def toString = "$" + id
 }
 
@@ -20,10 +20,6 @@ object Site {
   implicit def orderById: Order[Site] = orderBy((_:Site).id)
   implicit def orderingById = orderById.toScalaOrdering
   implicit def equalById: Equal[Site] = equalBy((_:Site).id)
-}
-
-object Site_ {
-  def unapply(s: Site) = Some((s.id,s.open))
 }
 
 
@@ -44,128 +40,89 @@ sealed trait PlaceGraph[A] {
   def map[B](f: A => B):PlaceGraph[B] = {
     PlaceGraph(
       forest.map((region: Region[A]) =>
-        region.map((tree: Tree[Site \/ A]) =>
-          tree.map((n: Site \/ A) =>
-            n.map(f)
-          )
+      region.map((tree: Tree[Site \/ A]) =>
+      tree.map((n: Site \/ A) => n.map(f))
+    )
+  )
+)
+}
+
+lazy val innerFace: Int = sites.size
+
+lazy val outerFace: Int = forest.size
+
+lazy val sites: Stream[Site] = forest.flatten.map((t:Tree[Site \/ A])=> t.flatten).flatten.filter((_:Site \/ A).isLeft).flatMap(s=> s.swap.toOption)
+
+def drawPlaceGraph: String = drawRegions(forest)
+private def drawRegions(rs: Stream[Region[A]]): String = rs.length match{
+  case 0 => ""
+  case n => drawRegions(rs.init) + drawTrees(n-1)(rs.last)
+}
+private def drawTrees(region: Int)(ts: Region[A]): String = "\n#" + region + "[\n" + ts.map(_.drawTree).foldLeft("")(_+_) + "]\n"
+
+def juxtapose(p: PlaceGraph[A]): PlaceGraph[A] = {
+  val rs = forest #::: {if (sites.isEmpty) {p.forest} else {incrementSites(p)(sites.max.id).forest}}
+  PlaceGraph(rs)
+}
+
+private def incrementSites(p: PlaceGraph[A])(inc: Int): PlaceGraph[A] = {
+  if (p.sites.max.id > inc) {p} else {
+    val rs = p.forest.map(region =>
+      region.map(tree =>
+        tree.map(n =>
+          n match {
+            case -\/(Site(i)) => -\/(Site(i+inc+1))
+            case n => n
+          }
         )
       )
     )
-  }
-
-  lazy val innerFace: Int = sites.size
-
-  lazy val outerFace: Int = forest.size
-
-  lazy val sites: Stream[Site] = forest.flatten.map((t:Tree[Site \/ A])=> t.flatten).flatten.filter((_:Site \/ A).isLeft).flatMap(s=> s.swap.toOption)
-
-  def drawPlaceGraph: String = drawRegions(forest)
-  private def drawRegions(rs: Stream[Region[A]]): String = rs.length match{
-    case 0 => ""
-    case n => drawRegions(rs.init) + drawTrees(n-1)(rs.last)
-  }
-  private def drawTrees(region: Int)(ts: Region[A]): String = "\n#" + region + "[\n" + ts.map(_.drawTree).foldLeft("")(_+_) + "]\n"
-
-  def juxtapose(p: PlaceGraph[A]): PlaceGraph[A] = {
-    val rs = forest #::: {if (sites.isEmpty) {p.forest} else {incrementSites(p)(sites.max.id).forest}}
     PlaceGraph(rs)
   }
+}
 
-  private def incrementSites(p: PlaceGraph[A])(inc: Int): PlaceGraph[A] = {
-    if (p.sites.max.id > inc) {p} else {
-      val rs = p.forest.map(region =>
-        region.map(tree =>
-          tree.map(n =>
-            n match {
-              case -\/(Site(i)) => -\/(Site(i+inc+1))
-              case n => n
-            }
-          )
-        )
-      )
-      PlaceGraph(rs)
-    }
-  }
-
-  def compose(p: PlaceGraph[A]): Option[PlaceGraph[A]] = {
-    if (innerFace != p.outerFace) {
-      None
-    } else {
-      PlaceGraph(
-        forest.map(region =>
-          region.map(t =>
-            composeRegionsInTree(t, p.closeAllSites)
-          )
-        )
-      ).openAllSites.some
-    }
-  }
-
-  private def closeAllSites: PlaceGraph[A] = {
+def compose(p: PlaceGraph[A]): Option[PlaceGraph[A]] = {
+  if (innerFace != p.outerFace) {
+    None
+  } else {
     PlaceGraph(
       forest.map(region =>
-        region.map(tree =>
-          tree.map(n =>
-            n match {
-              case -\/(Site_(i,true)) => -\/(Site(i)(false))
-              case n => n
-            }
-          )
-        )
+        region.flatMap(t => t match {
+          case Leaf(-\/(Site(i))) => p.forest(i)
+          case t => Stream(composeRegionsInSites(t, p, sites))
+        }
       )
     )
-  }
+  ).some
+}
+}
 
-  private def openAllSites: PlaceGraph[A] = {
-    PlaceGraph(
-      forest.map(region =>
-        region.map(tree =>
-          tree.map(n =>
-            n match {
-              case -\/(Site_(i,false)) => -\/(Site(i)(true))
-              case n => n
-            }
-          )
-        )
-      )
-    )
-  }
+//private def sitesInTree(tree: Tree[Site\/A]): Stream[Site] = tree.flatten.filter((_:Site \/ A).isLeft).flatMap(s=> s.swap.toOption)
 
- def findASite(loc: TreeLoc[Site \/ A]):Option[TreeLoc[Site \/ A]] = {
-    loc.find { (_:TreeLoc[Site \/ A]).getLabel match {
-      case -\/(Site(i)) => true
-      case _ => false
+private def insertRegionInLocation(loc: TreeLoc[Site \/ A],rg0: Region[A]):TreeLoc[Site \/ A] = {
+  rg0.length match {
+    case 0 => loc
+    case n => {
+      insertRegionInLocation(loc.insertLeft(rg0.head).right.get,rg0.tail)
     }
   }
-  }
+}
 
-  private def insertRegionInLocation(loc: TreeLoc[Site \/ A],rg0: Region[A]):TreeLoc[Site \/ A] = {
-    rg0.length match {
-      case 0 => loc
-      case n => {
-        insertRegionInLocation(loc.insertLeft(rg0.head).right.get,rg0.tail)
-      }
-    }
-  }
+private def composeRegionsInSites(tree: Tree[Site \/ A],pg: PlaceGraph[A],ss: Stream[Site]): Tree[Site \/ A] = {
+  ss.size match {
+    case 0 => tree
+    case n => {
+      tree.loc.find((_:TreeLoc[Site\/A]).getLabel == -\/(sites.head)) match {
+        case None => composeRegionsInSites(tree,pg,ss.tail)
+        case Some(loc) => {
+          val newTree = insertRegionInLocation(loc, pg.forest(ss.head.id)).delete.get.toTree
+          composeRegionsInSites(newTree,pg,ss.tail)
 
-  private def composeRegionsInTree(tree: Tree[Site \/ A],pg: PlaceGraph[A]): Tree[Site \/ A] = {
-    findASite(tree.loc) match {
-      case None => tree
-      case Some(p) => {
-        p.getLabel match {
-          case -\/(Site_(i,true)) => {
-            if (pg.outerFace > i){
-              val newTree = insertRegionInLocation(p, pg.forest(i)).delete.get.toTree
-              composeRegionsInTree(newTree,pg)
-            } else {
-              tree
-            }
-          }
-          case _ => tree
         }
       }
     }
   }
+}
 }
 
 object PlaceGraph extends PlaceGraphInstances{
