32565b08ba72590eb777175f1fce419456ca626b
Eloi Pereira <eloipereira@gmail.com>
2016-06-15 22:47:01 +0100

    solved variance problems. turned compose to a partial function

--- build.sbt
+++ build.sbt
@@ -8,11 +8,10 @@ resolvers ++= Seq(
   DefaultMavenRepository,
   "My bitbucket maven releases repo" at "https://bitbucket.org/eloipereira/maven-repo-releases/raw/master",
   "My bitbucket maven snapshots repo" at "https://bitbucket.org/eloipereira/maven-repo-snapshots/raw/master",
-  "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
-)
+  "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/")
 
 libraryDependencies ++= Seq(
-  "org.scalaz" %% "scalaz-core" % "7.2.2",
+  "org.scalaz" %% "scalaz-core" % "7.3.0-M2",
   "org.scalacheck" %% "scalacheck" % "1.13.0" % "test"
 )
 
--- src/main/scala/bigraph/PlaceGraph.scala
+++ src/main/scala/bigraph/PlaceGraph.scala
@@ -1,10 +1,5 @@
 package bigraph
 
-package object bigraph{
-  import scalaz._
-  type Region[A] = Stream[Tree[Site \/ A]]
-}
-
 import scalaz._
 import Scalaz._
 import scalaz.Tree._
@@ -23,14 +18,14 @@ object Site {
 }
 
 
-sealed trait PlaceGraph[A] {
-  import bigraph._
+ trait PlaceGraph[+A] {
+   self =>
   import Site._
 
-  def forest: Stream[Region[A]]
+  def forest[U >: A]: Stream[Stream[Tree[Site \/ U]]]
 
-  implicit def sh: Show[Site \/ A] = new Show[Site \/ A] {
-    override def shows(n: Site \/ A): String = {
+  implicit def sh: Show[Site \/ Any] = new Show[Site \/ Any] {
+    override def shows(n: Site \/ Any): String = {
       n match {
         case -\/(s) => s.toString
         case \/-(a) => a.toString
@@ -40,7 +35,7 @@ sealed trait PlaceGraph[A] {
 
   def map[B](f: A => B):PlaceGraph[B] = {
     PlaceGraph(
-      forest.map((_:Region[A]).map(_.map(_.map(f))))
+      forest.map((_:Stream[Tree[Site \/ A]]).map(_.map(_.map(f))))
     )
   }
 
@@ -52,22 +47,23 @@ sealed trait PlaceGraph[A] {
 
   def drawPlaceGraph: String = drawRegions(forest)
 
-  private def drawRegions(rs: Stream[Region[A]]): String =
+  private def drawRegions(rs: Stream[Stream[Tree[Site \/ Any]]]): String =
     rs.length match{
       case 0 => ""
       case n => drawRegions(rs.init) + drawTrees(n-1,rs.last)
     }
 
-  private def drawTrees(region: Int,ts: Region[A]): String = "\n#" + region + "[\n" + ts.map(_.drawTree).foldLeft("")(_+_) + "]\n"
+   private def drawTrees(region: Int,ts: Stream[Tree[Site \/ Any]]): String = "\n#" + region + "[\n" + ts.map(_.drawTree).foldLeft("")(_+_) + "]\n"
 
-  def juxtapose(p: PlaceGraph[A]): PlaceGraph[A] = {
-    val rs = forest #::: {if (sites.isEmpty) {p.forest} else {incrementSites(p)(sites.max.id).forest}}
+  def juxtapose[U >: A](p: PlaceGraph[U]): PlaceGraph[U] = {
+    val currentForest: Stream[Stream[Tree[Site \/ U]]] = self.forest
+    val rs = currentForest #::: {if (self.sites.isEmpty) {p.forest} else {incrementSites(p)(self.sites.max.id).forest}}
     PlaceGraph(rs)
   }
 
-  private def incrementSites(p: PlaceGraph[A])(inc: Int): PlaceGraph[A] = {
+  private def incrementSites[U >: A](p: PlaceGraph[U])(inc: Int): PlaceGraph[U] = {
     if (p.sites.max.id > inc) {p} else {
-      val rs = p.forest.map((region:Region[A]) =>
+      val rs = p.forest.map((region:Stream[Tree[Site \/ U]]) =>
         region.map(tree =>
           tree.map(n =>
             n match {
@@ -81,23 +77,19 @@ sealed trait PlaceGraph[A] {
     }
   }
 
-  def compose(p: PlaceGraph[A]): Option[PlaceGraph[A]] = {
-  if (innerFace != p.outerFace) {
-    None
-  } else {
-    PlaceGraph(
-      forest.map(region =>
-        region.flatMap(t => t match {
-          case Leaf(-\/(Site(i))) => p.forest(i)
-          case tree => Stream(composeRegionsInSites(tree, p, sites))
-        }
-      )
+def compose[U >: A]:PartialFunction[PlaceGraph[U],PlaceGraph[U]] = {
+  case p:PlaceGraph[U] if p.outerFace == self.innerFace => PlaceGraph(
+    forest.map((region:Stream[Tree[Site \/ U]]) =>
+      region.flatMap(t => t match {
+        case Leaf(-\/(Site(i))) => p.forest(i)
+        case tree => Stream(composeRegionsInSites(tree, p, sites))
+      }
     )
-  ).some
-}
+  )
+)
 }
 
-private def insertRegionInLocation(loc: TreeLoc[Site \/ A],rg0: Region[A]):TreeLoc[Site \/ A] = {
+private def insertRegionInLocation[U >: A](loc: TreeLoc[Site \/ U],rg0: Stream[Tree[Site \/ U]]):TreeLoc[Site \/ U] = {
   rg0.length match {
     case 0 => loc
     case n => {
@@ -106,75 +98,71 @@ private def insertRegionInLocation(loc: TreeLoc[Site \/ A],rg0: Region[A]):TreeL
   }
 }
 
-private def composeRegionsInSites(tree: Tree[Site \/ A],pg: PlaceGraph[A],ss: Stream[Site]): Tree[Site \/ A] = {
+private def composeRegionsInSites[U >: A](tree: Tree[Site \/ U],pg: PlaceGraph[U],ss: Stream[Site]): Tree[Site \/ U] = {
   ss.size match {
     case 0 => tree
     case n => {
-      tree.loc.find((_:TreeLoc[Site\/A]).getLabel == -\/(ss.head)) match {
+      tree.loc.find((_:TreeLoc[Site\/U]).getLabel == -\/(ss.head)) match {
         case None => composeRegionsInSites(tree,pg,ss.tail)
         case Some(loc) => {
           val newTree = insertRegionInLocation(loc, pg.forest(ss.head.id)).delete.get.toTree
           composeRegionsInSites(newTree,pg,ss.tail)
-
         }
       }
     }
   }
 }
+
 }
 
 object PlaceGraph extends PlaceGraphFunctions {
-  import bigraph._
-  def apply[A](s: Stream[Region[A]]): PlaceGraph[A] = new PlaceGraph[A]{
-    def forest = s
-    override def toString = "<placeGraph>"
-  }
-  def unapply[A](pg: PlaceGraph[A]): Option[Stream[Region[A]]] = Some(pg.forest)
+   def apply[A](s: Stream[Stream[Tree[Site \/ A]]]): PlaceGraph[A] = new PlaceGraph[A]{
+     def forest[U >: A]: Stream[Stream[Tree[Site \/ U]]] = s.asInstanceOf[Stream[Stream[Tree[Site \/ U]]]]
+     override def toString = "<placeGraph>"
+   }
+   def unapply[A](pg: PlaceGraph[A]): Option[Stream[Stream[Tree[Site \/ A]]]] = Some(pg.forest)
+ }
+
+case class Ion[A](n: A) extends PlaceGraph[A] with PlaceGraphFunctions{
+   def forest[U >: A]:Stream[Stream[Tree[Site \/ U]]] = ion(n).forest
 }
 
-case class Ion[A](n: A) extends PlaceGraph[A]{
-  import bigraph._
-  private def tree: Tree[Site \/ A] = Node(\/-(n),Stream(Leaf(-\/(Site(0)))))
-  override def forest: Stream[Region[A]] =
-    Stream(
-      Stream(
-        tree
-      )
-    )
+case class Atom[A](n:A) extends PlaceGraph[A]  with PlaceGraphFunctions{
+   def forest[U >: A]:Stream[Stream[Tree[Site \/ U]]] = atom(n).forest
 }
 
-case class Atom[A](n:A) extends PlaceGraph[A]{
-  import bigraph._
-  private def tree: Tree[Site\/A] = Node(\/-(n), Stream.empty)
-  override def forest: Stream[Region[A]] = Stream(Stream(tree))
+case object Empty extends PlaceGraph[Nothing]  with PlaceGraphFunctions{
+   def forest[U >: Nothing]: Stream[Stream[Tree[Site \/ U]]] = empty.forest
 }
 
-case object Empty extends PlaceGraph[Nothing]{
-  import bigraph._
-  private def empty: Stream[Tree[Site\/Nothing]] = Stream.Empty
-  override def forest = Stream(Stream.Empty)
+case object Permute extends PlaceGraph[Nothing]  with PlaceGraphFunctions{
+   def forest[U >: Nothing]: Stream[Stream[Tree[Site \/ U]]] = permute.forest
 }
 
-case object Permutation extends PlaceGraph[Nothing]{
-  import bigraph._
-  private def leaf0: Tree[Site \/ Nothing] = Leaf(-\/(Site(0)))
-  private def leaf1: Tree[Site \/ Nothing] = Leaf(-\/(Site(1)))
-  override def forest = Stream(Stream(leaf1),Stream(leaf0))
-}
-
-case object Join extends PlaceGraph[Nothing]{
-  import bigraph._
-  private def leaf0: Tree[Site \/ Nothing] = Leaf(-\/(Site(0)))
-  private def leaf1: Tree[Site \/ Nothing] = Leaf(-\/(Site(1)))
-  override def forest = Stream(Stream(leaf0,leaf1))
+case object Join extends PlaceGraph[Nothing] with PlaceGraphFunctions{
+   def forest[U >: Nothing]: Stream[Stream[Tree[Site \/ U]]] = join.forest
 }
 
 
 trait PlaceGraphFunctions{
-  implicit val s: Site = Site(0)
-  def empty[A]: PlaceGraph[A] = PlaceGraph(Stream(Stream.empty))
-  def permute[A]: PlaceGraph[A] = PlaceGraph(Stream(Stream(Leaf(-\/(Site(1)))),Stream(Leaf(-\/(Site(0))))))
-  def join[A]: PlaceGraph[A] = PlaceGraph(Stream(Stream(Leaf(-\/(Site(0))),Leaf(-\/(Site(1))))))
-  def atom[A](n: => A): PlaceGraph[A] = PlaceGraph(Stream(Stream(Node(\/-(n), Stream.empty))))
-  def ion[A](n: => A)(implicit s: Site): PlaceGraph[A] = PlaceGraph(Stream(Stream(Node(\/-(n),Stream(Leaf(-\/(s)))))))
+  private def leaf0[A]: Tree[Site \/ A] = Leaf(-\/(Site(0)))
+  private def leaf1[A]: Tree[Site \/ A] = Leaf(-\/(Site(1)))
+  private def leaf[A](n: => A): Tree[Site\/A] = Leaf(\/-(n))
+  private def nodeWithSite[A](n: => A): Tree[Site\/A] = Node(\/-(n),Stream(leaf0))
+
+  def empty[A]: PlaceGraph[A] = new PlaceGraph[A]{
+     def forest[U >: A]: Stream[Stream[Tree[Site \/ U]]] = Stream(Stream.empty)
+  }
+  def permute[A]: PlaceGraph[A] = new PlaceGraph[A]{
+     def forest[U >: A]: Stream[Stream[Tree[Site \/ U]]] = Stream(Stream(leaf1[U]),Stream(leaf0[U]))
+  }
+  def join[A]: PlaceGraph[A] = new PlaceGraph[A]{
+     def forest[U >: A]: Stream[Stream[Tree[Site \/ U]]] = Stream(Stream(leaf0[U],leaf1[U]))
+  }
+  def atom[A](n: => A): PlaceGraph[A] = new PlaceGraph[A]{
+     def forest[U >: A]: Stream[Stream[Tree[Site \/ U]]] = Stream(Stream(leaf(n)))
+  }
+  def ion[A](n: => A): PlaceGraph[A] = new PlaceGraph[A]{
+     def forest[U >: A]: Stream[Stream[Tree[Site \/ U]]] = Stream(Stream(nodeWithSite(n)))
+  }
 }
